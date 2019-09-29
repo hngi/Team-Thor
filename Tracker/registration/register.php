f3dd102a52b233c0890c869d0022b17326dae738
yusuf abdulrahman <aabdulrahmanyusuf@gmail.com>
2019-09-29 04:45:47 +0100

    added bootstrap js files to login and signup

--- Tracker/registration/register.php
+++ Tracker/registration/register.php
@@ -83,7 +83,8 @@
 		</form>
 	</div>
 
-	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+	<script src="../js/jquery-3.4.1.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
 </body>
 </html>
