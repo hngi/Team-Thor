037ed849f8d24bec69cddc8bd665f1cef5758431
yusuf abdulrahman <aabdulrahmanyusuf@gmail.com>
2019-09-29 04:09:07 +0100

    made final changes

--- Tracker/js/script.js
+++ Tracker/js/script.js
@@ -19,14 +19,16 @@ class goal {
     this.todos = todos
     }
 }
+
+// Document Raedy
+document.addEventListener('DOMContentLoaded', getFromLocalStorage)
+
 var i = 0;
-var j = 0;
 var goalIndex = 0
 let checked = 0
 let percentage = 0
 // let newGoal = new goal(`goal[i]`, goalInput.value)
 addGoal.addEventListener('click', () => {
-
     if (goalInput.value==""){
         alert("Enter a goal")
     }
@@ -35,26 +37,56 @@ addGoal.addEventListener('click', () => {
         const addTask = document.createElement('span')
         li.append(addTask)
         addTask.innerText = goalInput.value
-        ulGoals.append(addTask)
+        ulGoals.appendChild(addTask)
+        
+        // save in local storage
+        function getGoalFromLS() {
+            let inputGoals
+            const goalFromLS = localStorage.getItem('Goals')
+            if (goalFromLS === null) {
+                inputGoals = []
+            } else {
+                inputGoals = JSON.parse(goalFromLS)
+            }
+            return inputGoals
+        }       
+
+        
+
+        (function saveinLS (inputGoal) {
+            let inputGoals = getGoalFromLS()
+    
+            inputGoals.push(inputGoal)
+            localStorage.setItem('Goals', JSON.stringify(inputGoals))
+        })(goalInput.value)
+
+          // Clear Input Goal
+          goalInput.value = '';
+
+
         addTask.addEventListener("click", () => {
             goalIndex++;
             let newTask = new goal(`goal${goalIndex}`, goalInput.value)
             var goals = []
-            goals.push(newTask)
+            goals.push(newTask)            
+
             todoInput.style.display = "block";
             todoBtn.classList.remove('d-none');
-            goalInput.value = '';
             currentTask.innerText = `These are your To do lists for: ${addTask.innerText}` 
+
+            
         })
     }
 })
+
+
+
 todoForm.addEventListener("submit", () => {
     event.preventDefault()
     if (todoInput.value==""){
         alert("Enter a task")
     }
     else{
-        j++
         console.log(todoForm)
         i++;
         const label = document.createElement("label")
@@ -100,7 +132,31 @@ todoForm.addEventListener("submit", () => {
     }
 }) 
 
+// save in local storage
+function getGoalFromLS() {
+    let inputGoals
+    const goalFromLS = localStorage.getItem('Goals')
+    if (goalFromLS === null) {
+        inputGoals = []
+    } else {
+        inputGoals = JSON.parse(goalFromLS)
+    }
+    return inputGoals
+} 
 
+// Loads when document is ready and print courses into shopping cart
+function getFromLocalStorage() {
+    let goalsLS = getGoalFromLS()
+
+    // Loop through courses and print into cart
+    goalsLS.forEach(goal => {
+        const li = document.createElement('li')
+        const addTask = document.createElement('span')
+        li.append(addTask)
+        addTask.innerText = goal
+        ulGoals.appendChild(addTask)
+    });
+}
 
 
 
