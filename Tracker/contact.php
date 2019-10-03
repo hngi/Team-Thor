9538bc5a9c500562642c0dc15ae04264e8a665f0
Soot3 <53571625+Soot3@users.noreply.github.com>
2019-10-03 08:37:27 +0100

    Update contact.php

--- Tracker/contact.php
+++ Tracker/contact.php
@@ -1,13 +1,10 @@
 <?php
-/*
- *  CONFIGURE EVERYTHING HERE
- */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <demo@domain.com>';
+$from = '<hngincrediblethor@gmail.com>';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <demo@domain.com>';
+$sendTo = '<hngincrediblethor@gmail.com>';
 
 // subject of the email
 $subject = 'New message from contact form';
