ab229a46b07448227eb47b1d1b8d4e8e3bc4b220
NIXEL01 <54241219+NIXEL01@users.noreply.github.com>
2019-10-04 15:43:50 +0100

    Update register.php

--- Tracker/registration/register.php
+++ Tracker/registration/register.php
@@ -56,6 +56,29 @@
 					<div class="row d-flex justify-content-center align-items-center ">
 						<div class="col-8 mt-2 my-2 py-2">
 							<input type="email" name="email" value="<?php echo $email; ?>" class="form-control" placeholder="email">
+							<?php
+// Include library file
+require_once 'VerifyEmail.class.php';
+// Initialize library class
+$mail = new VerifyEmail();
+// Set the timeout value on stream
+$mail->setStreamTimeoutWait(20);
+// Set debug output mode
+$mail->Debug= TRUE;
+$mail->Debugoutput= 'html';
+// Set email address for SMTP request
+$mail->setEmailFrom('from@email.com');
+// Email to check
+$email = 'email@example.com';
+// Check if email is valid and exist
+if($mail->check($email)){
+   echo 'Email &lt;'.$email.'&gt; is exist!';
+}elseif(verifyEmail::validate($email)){
+   echo 'Email &lt;'.$email.'&gt; is valid, but not exist!';
+}else{
+   echo 'Email &lt;'.$email.'&gt; is not valid and not exist!';
+}
+?>
 						</div>
 					</div>
 					
